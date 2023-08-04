951e415066fc80669d45a401a5ba8fc854ef04a6
Justus-Becker-Rabbenstein <43116175+Justus-Becker-Rabbenstein@users.noreply.github.com>
2023-08-04 10:32:08 +0200

    Created ToDoDbModel which is the model for the database operations. It consists of 3 variables with getters and setters.

---
+++ src/main/java/com/example/myfirstrestapp/ToDoDbModel.java
@@ -0,0 +1,33 @@
+package com.example.myfirstrestapp;
+
+public class ToDoDbModel {
+    
+    private Integer id;
+    private String description;
+    private boolean isDone;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean getIsDone() {
+        return this.isDone;
+    }
+
+    public void setIsDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+
+}
