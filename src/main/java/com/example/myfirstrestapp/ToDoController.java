7c0b9ea987cf17a1eb0af815a5c768648b1a74ee
Justus-Becker-Rabbenstein <43116175+Justus-Becker-Rabbenstein@users.noreply.github.com>
2023-08-13 15:48:58 +0200

    added API secret auth for all ToDo API point

--- src/main/java/com/example/myfirstrestapp/ToDoController.java
+++ src/main/java/com/example/myfirstrestapp/ToDoController.java
@@ -14,6 +14,8 @@ public class ToDoController {
     // Dependency Injection des CRUD Operations Objekts
     @Autowired
     private ToDoRepository toDoRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     // responds to HTTP GET of /todo url
     @GetMapping("/todo")
@@ -33,9 +35,13 @@ public class ToDoController {
     }
 
     @GetMapping("/todo/all")
-    public ResponseEntity<Iterable<ToDo>> getAllToDo() {
-        Iterable<ToDo> allToDosInDb = toDoRepository.findAll();
-        return new ResponseEntity<Iterable<ToDo>>(allToDosInDb, HttpStatus.OK);
+    public ResponseEntity<Iterable<ToDo>> getAllToDo(@RequestHeader("api-secret") String secret) {
+        var userBySecret = userRepository.findBySecret(secret);
+        if (userBySecret.isPresent()) {
+            Iterable<ToDo> allToDosInDb = toDoRepository.findAllByUserId(userBySecret.get().getId());
+            return new ResponseEntity<Iterable<ToDo>>(allToDosInDb, HttpStatus.OK);
+        }
+        return  new ResponseEntity("Invalid API secret. ", HttpStatus.BAD_REQUEST);
     }
 
     // save new todo in database
